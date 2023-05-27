ab109430331f73c2886653eab9a28062cc8bd7ac
Adam Rose <adam.p.rose1@gmail.com>
2023-05-27 19:57:40 +0100

    yfinance + sql exploring

--- stock_data.py
+++ stock_data.py
@@ -3,7 +3,7 @@ Class for extracting stock data from yahoo finance
 
 """
 
-from .asset_cl import AssetData
+from .asset_base import AssetData
 
 import pandas as pd
 
@@ -14,3 +14,4 @@ class StockData(AssetData):
         
         super().__init__()
 
+#1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
